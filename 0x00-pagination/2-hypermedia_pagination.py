1aea82cc7831265333b1d87e8dc9fc1427c7bcc0
Anashm24 <anashoumaid19@gmail.com>
2024-08-06 00:19:47 +0100

    new

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -27,7 +27,7 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
-    
+
     def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
         """
         Takes 2 integer arguments and returns a tuple of size two
@@ -41,7 +41,7 @@ class Server:
         """
         end = page * page_size
         start = end - page_size
-        
+
         return (start, end)
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
@@ -54,9 +54,11 @@ class Server:
         Returns:
             List[List]: all the rows from the page
         """
-        assert isinstance(page, int) and page > 0, "page must be an integer greater than 0"
-        assert isinstance(page_size, int) and page_size > 0, "the page_size must be an integer greater than 0"
-        
+        assert isinstance(page, int) and page > 0, \
+            "page must be an integer greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, \
+            "the page_size must be an integer greater than 0"
+
         dataset = self.dataset()
         start, end = self.index_range(page, page_size)
         return dataset[start:end]
